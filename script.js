01dcc6a472559ee9c8fb5d4680eb0b25278b0417
Leandro Silveira <silveira.jedi@gmail.com>
2022-07-28 12:07:06 -0300

    Version 2

---
+++ script.js
@@ -0,0 +1,19 @@
+function load1() {
+    let load1 = document.querySelector(".load-1");
+    load1.style.display = "flex";
+}
+
+function close1() {
+    let load1 = document.querySelector(".load-1")
+    load1.style.display = "none";
+}
+
+function load2() {
+    let load2= document.querySelector(".load-2");
+    load2.style.display = "flex";
+}
+
+function close2() {
+    let load2 = document.querySelector(".load-2")
+    load2.style.display = "none";
+}
